cc2a30192a246db2a15f30db7f2c4dca629ca821
kyoung2-lee <kyoung60003@gmail.com>
2025-05-17 19:06:08 +0900

    Initial commit: Add version display application

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="version-display">
+        Current version: 0.1 (blue)
+      </div>
+    </div>
+  )
+}
+
+export default App
